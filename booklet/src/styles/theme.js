a78499a06139a37551d7895d7b0d13438fae7b1c
Baboya Choch <baboyachoch@gmail.com>
2022-11-05 03:04:01 -0500

    [frontend] created layout of site with mui grid

--- booklet/src/styles/theme.js
+++ booklet/src/styles/theme.js
@@ -5,10 +5,10 @@ const font = "Raleway, Arial, sans-serif";
 export const theme = createTheme({
     palette: {
         primary: {
-            main: '#674ea7',
+            main: '#674EA7',
         },
         secondary: {
-            main: '#ffd966',
+            main: '#FCCF22',
         },
     },
     typography: {
