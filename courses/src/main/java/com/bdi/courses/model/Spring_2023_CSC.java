697809a952de320a6723c98685dc4968c4b145e8
Dugyuck <bricetruong67@gmail.com>
2022-11-26 16:05:17 -0600

    added nested lab

--- courses/src/main/java/com/bdi/courses/model/Spring_2023_CSC.java
+++ courses/src/main/java/com/bdi/courses/model/Spring_2023_CSC.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -27,18 +28,22 @@ public class Spring_2023_CSC {
     private String moreinfo;
     private String specialenrollment;
     private int keey;
-    private boolean lab;
+    @OneToOne
+    private Spring_2023_CSC_Labs lab;
     private int labhash;
     private String prereqs;
     private String notes;
     private String description;
 
     public Spring_2023_CSC() {
-        
+
     }
-    public Spring_2023_CSC(int availability, int enrollment, String coursenum, String coursename, String type, int section,
+
+    public Spring_2023_CSC(int availability, int enrollment, String coursenum, String coursename, String type,
+            int section,
             int credits, String time, String days, String building, String instructor, String moreinfo,
-            String specialenrollment, int keey,boolean lab, int labhash, String prereqs, String notes, String description) {
+            String specialenrollment, int keey, Spring_2023_CSC_Labs lab, int labhash, String prereqs, String notes,
+            String description) {
         super();
         this.availability = availability;
         this.enrollment = enrollment;
@@ -60,123 +65,163 @@ public class Spring_2023_CSC {
         this.notes = notes;
         this.description = description;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public int getAvailability() {
         return availability;
     }
+
     public void setAvailability(int availability) {
         this.availability = availability;
     }
+
     public int getEnrollment() {
         return enrollment;
     }
+
     public void setEnrollment(int enrollment) {
         this.enrollment = enrollment;
     }
+
     public String getCoursenum() {
         return coursenum;
     }
+
     public void setCoursenum(String coursenum) {
         this.coursenum = coursenum;
     }
+
     public String getCoursename() {
         return coursename;
     }
+
     public void setCoursename(String coursename) {
         this.coursename = coursename;
     }
+
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
+
     public int getSection() {
         return section;
     }
+
     public void setSection(int section) {
         this.section = section;
     }
+
     public int getCredits() {
         return credits;
     }
+
     public void setCredits(int credits) {
         this.credits = credits;
     }
+
     public String getTime() {
         return time;
     }
+
     public void setTime(String time) {
         this.time = time;
     }
+
     public String getDays() {
         return days;
     }
+
     public void setDays(String days) {
         this.days = days;
     }
+
     public String getBuilding() {
         return building;
     }
+
     public void setBuilding(String building) {
         this.building = building;
     }
+
     public String getInstructor() {
         return instructor;
     }
+
     public void setInstructor(String instructor) {
         this.instructor = instructor;
     }
+
     public String getMoreinfo() {
         return moreinfo;
     }
+
     public void setMoreinfo(String moreinfo) {
         this.moreinfo = moreinfo;
     }
+
     public String getSpecialenrollment() {
         return specialenrollment;
     }
+
     public void setSpecialenrollment(String specialenrollment) {
         this.specialenrollment = specialenrollment;
     }
+
     public int getKeey() {
         return keey;
     }
+
     public void setKeey(int keey) {
         this.keey = keey;
     }
-    public boolean isLab() {
+
+    public Spring_2023_CSC_Labs isLab() {
         return lab;
     }
-    public void setLab(boolean lab) {
+
+    public void setLab(Spring_2023_CSC_Labs lab) {
         this.lab = lab;
     }
+
     public int getLabhash() {
         return labhash;
     }
+
     public void setLabhash(int labhash) {
         this.labhash = labhash;
     }
+
     public String getPrereqs() {
         return prereqs;
     }
+
     public void setPrereqs(String prereqs) {
         this.prereqs = prereqs;
     }
+
     public String getNotes() {
         return notes;
     }
+
     public void setNotes(String notes) {
         this.notes = notes;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
