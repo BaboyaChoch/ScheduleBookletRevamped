b27ffe732ea6e7296a9190485cf518053f9d450f
Dugyuck <bricetruong67@gmail.com>
2022-11-27 13:14:37 -0600

    added lab_placeholder for courses with no lab

---
+++ courses/src/main/java/com/bdi/courses/repository/Lab_Placeholder_Repository.java
@@ -0,0 +1,9 @@
+package com.bdi.courses.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bdi.courses.model.Lab_Placeholder;
+
+public interface Lab_Placeholder_Repository extends JpaRepository<Lab_Placeholder, Long> {
+
+}
